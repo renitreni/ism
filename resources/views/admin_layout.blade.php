7098f834206a1b2d0c4637f0c23bf415e8c8d4fe
Renier Trenuela <renier.trenuela@gmail.com>
2023-02-05 15:21:05 +0800

    update

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -55,7 +55,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; IT Rockstars 2019</span>
+                    <span>Copyright &copy; IT Rockstar 2019</span>
                 </div>
             </div>
         </footer>
