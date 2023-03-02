1075bee579ed16068ca111ebbc0e5ea81efb76bd
Renier Trenuela <renier.trenuela@gmail.com>
2023-03-02 23:44:43 +0800

    Update top-sales-agent-livewire.blade.php

--- resources/views/livewire/top-sales-agent-livewire.blade.php
+++ resources/views/livewire/top-sales-agent-livewire.blade.php
@@ -4,28 +4,35 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-6">
+                        <div class="col-6 mb-3">
                             <label>Month</label>
                             <input type="text" wire:model="month" class="form-control">
+                            <select wire:model="month" id="">
+                                
+                            </select>
                         </div>
-                        <div class="col-6">
+                        <div class="col-6 mb-3">
                             <label>Year</label>
                             <input type="number" wire:model="year" class="form-control">
                         </div>
                         <div class="col-12">
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-6 mb-2">
                                     Sales Count
                                 </div>
-                                <div class="col-6">
+                                <div class="col-6 mb-2">
                                     Name
                                 </div>
                                 @foreach ($tops as $value)
                                     <div class="col-6">
-                                        {{ $value['sales'] }}
+                                        <strong>
+                                            {{ $value['sales'] }}
+                                        </strong>
                                     </div>
                                     <div class="col-6">
-                                        {{ $value['name'] }}
+                                        <strong>
+                                            {{ $value['name'] }}
+                                        </strong>
                                     </div>
                                 @endforeach
                             </div>
