03d833e52e79ca61f17f55505fa37ba4561a4d95
Renier <17608701+renitreni@users.noreply.github.com>
2023-10-12 21:58:02 +0800

    Update 2023_10_11_063523_add_column_audit_logs.php

--- database/migrations/2023_10_11_063523_add_column_audit_logs.php
+++ database/migrations/2023_10_11_063523_add_column_audit_logs.php
@@ -8,14 +8,13 @@ class AddColumnAuditLogs extends Migration
 {
     /**
      * Run the migrations.
-     * php artisan make:migration add_column_audit_logs --table=audit_logs
-     *  2023_10_11_063523_add_column_audit_logs
+     * php artisan migrate --path=database/migrations/2023_10_11_063523_add_column_audit_logs
+     *  
      * @return void
      */
     public function up()
     {
         Schema::table('audit_logs', function (Blueprint $table) {
-            //
             $table->string("action_id",100)->nullable();
             $table->string("action",100)->nullable();
             $table->string("current",100)->nullable();
@@ -30,8 +29,11 @@ class AddColumnAuditLogs extends Migration
      */
     public function down()
     {
-        Schema::table('audit_logs', function (Blueprint $table) {
-            //
+        Schema::table('audit_logs', function (Blueprint $table) { 
+            $table->dropColumn("action_id",100);
+            $table->dropColumn("action",100);
+            $table->dropColumn("current",100);
+            $table->dropColumn("previous",100);
         });
     }
 }
