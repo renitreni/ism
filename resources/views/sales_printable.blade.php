254ea79684dfbc0de4c0293c553b0e20ca050f21
busybee-renier <140514550+busybee-renier@users.noreply.github.com>
2023-12-07 20:28:33 +0800

    update

--- resources/views/sales_printable.blade.php
+++ resources/views/sales_printable.blade.php
@@ -84,7 +84,7 @@
                 </tr>
                 <tr>
                     <td>Date:</td>
-                    <td class="header-content">{{ \Carbon\Carbon::parse($sales_order->created_at)->format('F j, Y') }}</td>
+                    <td class="header-content">{{ \Carbon\Carbon::parse($sales_order->due_date)->format('F j, Y') }}</td>
                 </tr>
                 </tbody>
             </table>
