df62a1bd17ce732c8ad7e18ed974a0215340ad84
busybeeMarvin <131327257+busybeeMarvin@users.noreply.github.com>
2024-02-25 20:09:45 +0800

    Fix Vat / Add Debug Image

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -153,6 +153,37 @@
             </div>
         </div>
 
+        <div id="vatTypeModal" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Edit Vat Type</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label class="control-label">Pick a type</label>
+                                    <select class="form-control" v-model="overview.vat_type">
+                                        <option value="">-- Select Options --</option>
+                                        <option value="VAT EX">VAT EX</option>
+                                        <option value="VAT INC">VAT INC</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" @click="updateVat">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div id="deliveryStatusModal" class="modal fade" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
