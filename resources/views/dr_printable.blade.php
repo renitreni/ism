963bda022fe6d55c9c4450b3fcfd7674a518bc66
busybeeMarvin <131327257+busybeeMarvin@users.noreply.github.com>
2024-02-26 07:41:09 +0800

    FIXED IMAGE HEADER

--- resources/views/dr_printable.blade.php
+++ resources/views/dr_printable.blade.php
@@ -106,7 +106,8 @@
             </table>
         </td>
         <td width="20%">
-            {{-- <img src="{{ asset($print_setting->header_logo_path . $print_setting->header_logo) }}" style="width: auto; height: 86px;"> --}}
+            {{-- <img src="{{ public_path('app/public/print/header/1_ITR_CORPORATION_LOGO.jpg') }}" height="150"> --}}
+            <img src="{{ public_path('app/public/print/header/'.$print_setting->header_logo) }}" height="150">
         </td>
     </tr>
     </tbody>
